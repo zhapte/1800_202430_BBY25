20669dc917a7dfc5d27e094205038ba64869e880
Lucas Liu <lucasqhliu@gmail.com>
2024-11-04 22:00:40 -0800

    finished add event function

--- scripts/addevent.js
+++ scripts/addevent.js
@@ -1,14 +1,38 @@
 
 function hello(){
     var eventRef = db.collection("customevents");
-
+    let evenowner;
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            eventRef.add({
+                eventID: "bby01",
+                eventOwner: user.uid,
+                goal: document.getElementById('eventgoal').value,
+                name: document.getElementById('eventtitle').value ,
+                groupSize: document.getElementById('eventsize').value,
+                eventdes: document.getElementById('eventdes').value,
+                last_updated: firebase.firestore.FieldValue.serverTimestamp()
+            }).then(function () {
+                console.log("event added to database");
+                window.location.assign("main.html");       //re-direct to main.html after event added
+            })
+        } else {
+            console.log("No user is logged in."); // Log a message when no user is logged in
+        }
+    })
+    console.log(evenowner);
+    /*
     eventRef.add({
         eventID: "bby01",
-        name: "custom event",
+        eventOwner: evenowner,
+        goal: document.getElementById('eventgoal').value,
+        name: document.getElementById('eventtitle').value ,
+        groupSize: document.getElementById('eventsize').value,
+        eventdes: document.getElementById('eventdes').value,
         last_updated: firebase.firestore.FieldValue.serverTimestamp()
     }).then(function () {
-        console.log("New user added to firestore");
+        console.log("event added to database");
         window.location.assign("main.html");       //re-direct to main.html after event added
-    })
+    })*/
 
 }
