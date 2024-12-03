6bbbba7ef07341564b406e27702c3a723a993060
Lucas Liu <lucasqhliu@gmail.com>
2024-12-02 22:31:13 -0800

    added comment to the js files.

--- scripts/cityevent.js
+++ scripts/cityevent.js
@@ -3,6 +3,7 @@ function loadEvents() {
     eventTableBody.innerHTML = ""; // Clear existing rows
 
     // code sourced from a combination of Carly's demo and Aaron's example
+    //uses for each loop to get every document in the city event and populate the table on the page.
     db.collection("cityevents").get().then((snapshot) => {
         snapshot.forEach((doc) => {
             const event = doc.data();
@@ -14,6 +15,7 @@ function loadEvents() {
             row.setAttribute("data-id", event.name);
 
             // Populate row with event data
+            //map each data to the specific table.
             row.innerHTML = `
                 <td>${event.name}</td>
                 <td>${event.groupSize}</td>
@@ -24,6 +26,7 @@ function loadEvents() {
 
 
             row.addEventListener("click", () => {
+                //adde event listener tothe rows so that when it is clicked it will be passed to the detail page.
                 window.location.href = `events.html?docId=${doc.id}&database=cityevents`;
             });
 
